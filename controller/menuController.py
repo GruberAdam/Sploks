659b823a0c1e015df2a046b8ed7890d15bf61c67
Adam Gruber <adam.gruber@cpnv.ch>
2022-02-17 15:56:35 +0100

    Main window is now displayed (buttons have event listener too)

--- controller/menuController.py
+++ controller/menuController.py
@@ -1,3 +1,33 @@
+# Author : Adam Gruber
+# Date : 17.02.2022
+# Version : 1.0
+
+
 from PyQt5 import QtWidgets, QtGui, uic
+
+def displayMainMenu(self):
+    mainMenuWindow = uic.loadUi("view/menuView.ui", self)
+
+    # Click event listener 
+    mainMenuWindow.btnClients.clicked.connect(displayClients)
+    mainMenuWindow.btnStock.clicked.connect(displayStock)
+    mainMenuWindow.btnStaff.clicked.connect(displayStaff)
+    mainMenuWindow.btnContracts.clicked.connect(displayContracts)
+
+    mainMenuWindow.show()
+
+
+def displayClients(self):
+    print("in Clients")
+
+def displayStock(self):
+    print("in Stock")
+
+def displayStaff(self):
+    print("in Staff")
+
+def displayContracts(self):
+    print("in Contracts")
+
 
 
--- sploks.py
+++ sploks.py
@@ -1,16 +1,19 @@
+# Author : Adam Gruber
+# Date : 17.02.2022
+# Version : 1.0
+
 import sys
 import os
 sys.path.append(os.getcwd() + "\\packages") # Importing packages file
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
 
-from controller import *
+from controller import menuController
 
 class Sploks(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        menuController
-        uic.loadUi("view/menuView.ui", self)
-        self.show()
+        menuController.displayMainMenu(self) # Calls function in controller
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Sploks()
