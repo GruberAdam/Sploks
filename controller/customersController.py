6e05f5d729572ec6a12a20b0a90146aa116a64c8
Adam Gruber <adam.gruber@cpnv.ch>
2022-03-10 17:06:35 +0100

    when user doubles clicks in the client window, it gets the id of the user

--- controller/customersController.py
+++ controller/customersController.py
@@ -1,23 +1,45 @@
-from PyQt5 import QtWidgets, QtGui, uic
+from PyQt5 import QtWidgets, QtGui, uic, QtCore
 from model.customersModel import *
 class CustomersUi(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.customersWindow = uic.loadUi("view/customersView.ui", self)
+        self.customersWindow = uic.loadUi("view/customersView.ui", self) 
 
         self.customers = getCustomers(self)
-        
-        for customer in self.customers:
-            self.customersWindow.tableCustomers.insertRow(customer[0] - 1)
 
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 0, QtWidgets.QTableWidgetItem(str(customer[1]))) # Sets the ID
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 1, QtWidgets.QTableWidgetItem(str(customer[2]))) # Sets the LastName
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 2, QtWidgets.QTableWidgetItem(str(customer[3]))) # Sets the FirstName
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 3, QtWidgets.QTableWidgetItem(str(customer[8]))) # Sets the NPA
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 4, QtWidgets.QTableWidgetItem(str(customer[9]))) # Sets the Locality
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 5, QtWidgets.QTableWidgetItem(str(customer[4]))) # Sets the Phone number
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 6, QtWidgets.QTableWidgetItem(str(customer[5]))) # Sets the email
-            self.customersWindow.tableCustomers.setItem(customer[0] - 1, 7, QtWidgets.QTableWidgetItem(str(customer[6]))) # Sets the mobile phone
-        
+        self.customersWindow.tableCustomers.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
+
+        for index, customer in enumerate(self.customers):
+            self.customersWindow.tableCustomers.verticalHeader().setVisible(False) # Hides the row number
+            self.customersWindow.tableCustomers.insertRow(index) # Inserts the row
+
+            # Fills the table
+            self.customersWindow.tableCustomers.setItem(index, 0, QtWidgets.QTableWidgetItem(str(customer[0]))) # Sets the ID
+            self.customersWindow.tableCustomers.setItem(index, 1, QtWidgets.QTableWidgetItem(str(customer[1]))) # Sets the LastName
+            self.customersWindow.tableCustomers.setItem(index, 2, QtWidgets.QTableWidgetItem(str(customer[2]))) # Sets the FirstName
+            self.customersWindow.tableCustomers.setItem(index, 3, QtWidgets.QTableWidgetItem(str(customer[3]))) # Sets the Address
+            self.customersWindow.tableCustomers.setItem(index, 4, QtWidgets.QTableWidgetItem(str(customer[8]))) # Sets the NPA
+            self.customersWindow.tableCustomers.setItem(index, 5, QtWidgets.QTableWidgetItem(str(customer[9]))) # Sets the Locality
+            self.customersWindow.tableCustomers.setItem(index, 6, QtWidgets.QTableWidgetItem(str(customer[4]))) # Sets the Phone number
+            self.customersWindow.tableCustomers.setItem(index, 7, QtWidgets.QTableWidgetItem(str(customer[5]))) # Sets the email
+            self.customersWindow.tableCustomers.setItem(index, 8, QtWidgets.QTableWidgetItem(str(customer[6]))) # Sets the mobile phone
+
+        self.customersWindow.tableCustomers.viewport().installEventFilter(self) # Event listener
         print("Customer data loaded")
+        
         self.customersWindow.show()
+
+    def eventFilter(self, source, event):
+        if self.customersWindow.tableCustomers.selectedIndexes() != []: # Checks that the user clicked on a cell
+            if event.type() == QtCore.QEvent.MouseButtonDblClick: # If user double clicked
+
+                row = self.customersWindow.tableCustomers.currentRow() # gets row clicked
+                id = self.customersWindow.tableCustomers.item(row, 0).text() # gets ID based on click
+                
+
+                
+
+                
+            
+        return False
+ 
